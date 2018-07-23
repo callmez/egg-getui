4b49def70dec2387c7971653fe0dfdcd71bde10c
bugall <bugall@sina.cn>
2018-07-23 22:54:27 +0800

    first commit

---
+++ app/extend/application.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const GeTui = require('../../getui/');
+
+module.exports = {
+  get geTui() {
+    const gtConfig = this.config.geTui;
+    GeTui.init(gtConfig)
+
+    return {
+      pushMessageToApp: GeTui.pushMessageToApp,
+    };
+  },
+};
